7e81f80a55ee0c987c23b54871add140d2b2a64e
John Matzakos <23533533+johnmatzakos@users.noreply.github.com>
2021-07-14 17:21:04 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+# Author: Ioannis Matzakos | Date: 05/02/2020
+
+from utilities import logger
+import time
+
+# configure logger
+log = logger.setup_logger("setup")
+
+log.info("Start of Execution\n\n\t\t\t\t---------- PROPAGANDA RECOGNITION: SETUP ----------\n")
+
+# start calculating execution time
+start_time = time.time()
+
+# install the required python modules
+# pip install dependencies.txt
+
+# stop calculating execution time
+end_time = time.time()
+total_time = end_time - start_time
+log.info(f"Execution Time: {total_time} seconds")
+
+log.info("Start of Execution\n\n\t\t\t\t---------- SETUP COMPLETED SUCCESSFULLY ----------\n")

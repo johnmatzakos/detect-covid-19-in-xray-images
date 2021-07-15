574c03b22c7a60bf18e537f246a781c9431fe261
John Matzakos <23533533+johnmatzakos@users.noreply.github.com>
2021-07-15 11:19:52 +0300

    Update date_utils.py

--- utilities/date_utils.py
+++ utilities/date_utils.py
@@ -6,5 +6,4 @@ from datetime import datetime
 # Getting the current timestamp (date and time) of the system at the given moment, displays it and returns it
 def get_timestamp():
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    print(f"timestamp: {timestamp}")
     return timestamp

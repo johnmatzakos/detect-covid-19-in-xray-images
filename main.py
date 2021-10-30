fc935c4694c32a4580ffd558d467d0627afaf7ce
John Matzakos <ioannis.matzakos@gmail.com>
2021-10-30 20:07:29 +0300

    added logs and changed model save

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 # Author: John Matzakos
 
 from sklearn.model_selection import train_test_split
-import pickle
-
 
 from data_mining import *
 from data_preprocessing import *
 from evaluation import *
 from information_visualization import *
-from utilities import logger
-from utilities.date_utils import get_timestamp
-
-log = logger.setup_logger("main")
 
 # Defining dataset based on working directory
 dataset_path = "dataset"
@@ -42,13 +36,10 @@ model = vgg_16(weights, include_top, height, width, number_of_channels, number_o
 # place the model head on top of the basic model
 model = get_model_head(model)
 
-# Get model summary
-log.info(model.summary())
-
 # compile the constructed model
 model = compile_model(model, learning_rate, epochs)
 
-# freeze all vgg-16 layers so only the custom model will be trained
+# freeze all vgg-16 layers so only the custom model hear will be trained
 model = freeze_layers(model)
 
 # training the CNN Model
@@ -57,19 +48,9 @@ trained_model = train_model(model, trainX, trainY, testX, testY, batch_size, epo
 # Evaluation Phase
 execute_evaluation(model, testX, testY, batch_size, lb)
 
-# Get current date and time in timestamp format
-timestamp = get_timestamp()
-
 # Information Visualization Phase
-execute_visualization(epochs, trained_model, f"visualizations/vgg16_training_loss_and_accuracy_{timestamp}.png")
-plot_ann_architecture(trained_model, f"visualizations/vgg16_{timestamp}.png")
+execute_visualization(epochs, trained_model)
 
 # Serialize the model, save it for deployment
-log.info("Saving model...")
-model.save(f"models/covid19_vgg16_model_{timestamp}.h5", save_format="h5")
-
-# Save the model to a .pkl file
-Pkl_Filename = f"models/covid19_vgg16_model_{timestamp}.pkl"
-
-with open(Pkl_Filename, 'wb') as file:
-    pickle.dump(model, file)
+print("Saving model...")
+model.save("covid19_vgg16_model", save_format="hdf5")
